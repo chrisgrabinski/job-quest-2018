1e52ab30d392b5003ff6a51f5448be6dfbe7c004
Christopher Grabinski <christopher@unbabel.com>
2019-03-19 23:33:20 +0000

    Use Prettier

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 const App = () => {
   const [joke, setJoke] = useState("Joke");
 
   function fetchJoke() {
-    fetch('http://api.icndb.com/jokes/random/')
-      .then(function (response) {
+    fetch("http://api.icndb.com/jokes/random/")
+      .then(function(response) {
         return response.json();
       })
-      .then(function (myJson) {
-        setJoke(myJson.value.joke)
+      .then(function(myJson) {
+        setJoke(myJson.value.joke);
       });
   }
 
@@ -19,14 +19,11 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {joke}
-          </p>
+        <p>{joke}</p>
         <button onClick={fetchJoke}>Fetch Joke</button>
-
       </header>
     </div>
   );
-}
+};
 
 export default App;
