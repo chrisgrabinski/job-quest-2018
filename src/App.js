87d38caf2ab628058648f3d376757a2056b12d75
Christopher Grabinski <christopher@unbabel.com>
2019-03-20 00:00:23 +0000

    Added user input for first/last name

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import logo from "./logo.svg";
 import "./App.css";
 
 const App = () => {
-  const [joke, setJoke] = useState("Joke");
+  const [joke, setJoke] = useState();
+  const [firstName, setFirstName] = useState("Chuck");
+  const [lastName, setLastName] = useState("Norris");
 
   function fetchJoke() {
-    fetch("http://api.icndb.com/jokes/random/")
+    fetch(
+      `http://api.icndb.com/jokes/random/?${firstName &&
+        `firstName=${firstName}`}${firstName && lastName && `&`}${lastName &&
+        `lastName=${lastName}`}`
+    )
       .then(function(response) {
         return response.json();
       })
@@ -19,6 +25,28 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <form action="">
+          <div>
+            <label>
+              First name
+              <input
+                type="text"
+                name="firstName"
+                value={firstName}
+                onChange={event => setFirstName(event.target.value)}
+              />
+            </label>
+            <label>
+              Last name
+              <input
+                type="text"
+                name="lastName"
+                value={lastName}
+                onChange={event => setLastName(event.target.value)}
+              />
+            </label>
+          </div>
+        </form>
         <p>{joke}</p>
         <button onClick={fetchJoke}>Fetch Joke</button>
       </header>
