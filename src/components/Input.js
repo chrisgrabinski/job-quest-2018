155b84c706db5a9cc8fa9a43cd5bbdb1feb77ccf
Christopher Grabinski <christopher@unbabel.com>
2019-03-23 09:52:49 +0000

    Added input styles with emotion

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,10 +1,33 @@
 import React from "react";
+import { css } from "emotion";
 
 const Input = ({ type, label, name, value, onChange }) => {
   return (
     <label>
-      <span>{label}</span>
-      <input type={type} name={name} value={value} onChange={onChange} />
+      <span
+        className={css`
+          display: block;
+          font-size: 12px;
+          margin-bottom: 4px;
+        `}
+      >
+        {label}
+      </span>
+      <input
+        type={type}
+        name={name}
+        value={value}
+        onChange={onChange}
+        className={css`
+          border-radius: 5px;
+          border: 0;
+          box-sizing: border-box;
+          display: block;
+          height: 48px;
+          padding: 0 16px;
+          width: 100%;
+        `}
+      />
     </label>
   );
 };
